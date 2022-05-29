9184efbdb159d61e57c3f4848a4acbf31d7cf1ab
Francis_ldn <fnhh04@gmail.com>
2022-05-29 16:54:02 +0100

    add protection for api keys

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,13 @@ import {getFirestore} from 'firebase/firestore';
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDybz7udVpWSFQwayBFC9BVkN4IVjEGqos",
-  authDomain: "netflix-clone-b6e0c.firebaseapp.com",
-  projectId: "netflix-clone-b6e0c",
-  storageBucket: "netflix-clone-b6e0c.appspot.com",
-  messagingSenderId: "544396408644",
-  appId: "1:544396408644:web:232517a8437a2223d9c911",
-  measurementId: "G-YKQ4N3L908"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain:  process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:  process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:  process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId:  process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // check if the app is initialised
